3a761fd578a82906ab93f3bb68552c995eb40b97
unknown <abhishekmali7413@gmail.com>
2025-11-12 14:45:45 +0400

    user 3

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 # Create the main window
 root = tk.Tk()
-chatbot = ChatBot('Chatty')
+chatbot = ChatBot("Chatty")
 root.title("Simple Tkinter Program")
 root.geometry("300x200")
 
@@ -12,20 +12,22 @@ root.geometry("300x200")
 label = tk.Label(root, text="Hello, Tkinter!", font=("Arial", 14))
 label.pack(pady=20)
 
+
 # Define a function for button click
 def on_click():
     label.config(text="Button Clicked!")
 
+
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Train the chatbot with the English corpus
-trainer.train('chatterbot.corpus.english')
+trainer.train("chatterbot.corpus.english")
 
 print("Chatty is ready to talk! Type 'bye' to exit.")
 
 while True:
     user_input = input("You: ")
-    if user_input.lower() == 'bye':
+    if user_input.lower() == "bye":
         print("Chatty: Goodbye!")
         break
 
@@ -39,5 +41,3 @@ button.pack(pady=10)
 
 # Run the Tkinter event loop
 root.mainloop()
-
-
